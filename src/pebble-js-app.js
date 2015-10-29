215af945b566042ddc953449cd29fb12b6d84660
wildhart <cmorison@gmail.com>
2015-10-29 14:09:38 +1300

    Use cached platforms every time, tidy alarm window

--- src/pebble-js-app.js
+++ src/pebble-js-app.js
@@ -264,10 +264,15 @@ function distance(lat1, lon1, lat2, lon2) { // all parameters should already be 
 function get_platforms(reason) {
   var date=new Date();
   var old_date=localStorage.getItem("platforms_date");
-  if (old_date && (date-old_date) < 7 * 24 * 60 * 60 * 1000 ) { // if data is less than 7 days old (in milliseconds)
-    console.log("Using cached platform data");
+  if (old_date) {
+    console.log("Parsing cached platform data");
     parse_platforms(localStorage.getItem('platforms'), reason);
-    return;
+    if ((date-old_date) < 7 * 24 * 60 * 60 * 1000 ) { // if data is less than 7 days old (in milliseconds)
+      return;
+    } else {
+      console.log("Cache is old.");
+      reason='';
+    }
   }
   var req = new XMLHttpRequest();
   req.open('GET', 'http://rtt.metroinfo.org.nz/rtt/public/utility/file.aspx?ContentType=SQLXML&Name=JPPlatform', true);
@@ -276,16 +281,13 @@ function get_platforms(reason) {
     if (req.readyState == 4) {
       if(req.status == 200) {
         //console.log('AJAX Resonse: ' + req.responseText);
+        console.log("Caching new platform data...");
         localStorage.setItem('platforms',req.responseText);
         localStorage.setItem('platforms_date',date.valueOf()); // milliseconds
         console.log("Parsing new platform data...");
         parse_platforms(req.responseText,reason);
       } else {
         console.log("Couldn't fetch platforms (status="+req.status+")");
-        if (old_date) {
-          console.log("Using cached platform data");
-          parse_platforms(localStorage.getItem('platforms'),reason);
-        }
       }
     }
   };
@@ -307,5 +309,7 @@ function parse_platforms(platforms, reason) {
     obj.Long *= deg2rad;
     platformsOBJ[obj.PlatformNo]=obj;
   }
-  if (reason=='location') get_location(); else check_platform(reason);
+  if (reason) {
+    if (reason=='location') get_location(); else check_platform(reason);
+  }
 }
